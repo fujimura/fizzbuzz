ed9164f0f56e6636287a1c2c66ae2e0d06b44ae6
Fujimura Daisuke <dfujimura@aiming-inc.com>
2012-04-24 17:00:57 +0900

    Haskell

---
+++ Haskell/FizzBuzz.hs
@@ -0,0 +1,11 @@
+fb :: Int -> IO ()
+fb i = mapM_ (print . fb') [1..i]
+
+fb' :: Int -> String
+fb' i | i `mod` 15 == 0 = "FizzBuzz"
+      | i `mod` 3  == 0 = "Fizz"
+      | i `mod` 5  == 0 = "Buzz"
+      | otherwise      = (show i)
+
+main :: IO ()
+main = fb 30
